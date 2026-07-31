06bdf112ce684d539b44b8146c6a375b995a3d61
Ninitha-K <ninithakumar1472006@gmail.com>
2026-07-31 09:10:25 +0530

    File updated at 31/07/2026, 09:10:14

--- 113-PathSumII/113-PathSumII.java
+++ 113-PathSumII/113-PathSumII.java
@@ -1,37 +1,24 @@
-// Last updated: 31/07/2026, 09:08:06
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-18        ArrayList<List<Integer>> li = new ArrayList<>();
-19        ArrayList<Integer> a = new ArrayList<>();
-20        dfs(root,targetSum,0,li,a);
-21        return li;
+// Last updated: 31/07/2026, 09:10:14
+1class Solution {
+2    public int numDistinct(String s, String t) {
+3        int n = s.length();
+4        int m = t.length();
+5
+6        int[][] dp = new int[n+1][m+1];
+7
+8        for(int i = 0; i <= n; i++){
+9            dp[i][0] = 1;
+10        }
+11
+12        for(int i = 1; i <= n; i++){
+13            for(int j = 1; j <= m; j++){
+14                if(s.charAt(i-1) == t.charAt(j-1)){
+15                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+16                }else{
+17                    dp[i][j] = dp[i-1][j];
+18                }
+19            }
+20        }
+21        return dp[n][m];
 22    }
-23    void dfs(TreeNode root, int targetSum,int sum,List<List<Integer>> li,List<Integer> a ){
-24        if(root==null){
-25            return;
-26        }
-27        sum +=root.val;
-28        a.add(root.val);
-29        if(root.left==null&&root.right==null && sum==targetSum){
-30            li.add(new ArrayList<>(a));
-31        }
-32        dfs(root.left,targetSum,sum,li,a);
-33        dfs(root.right,targetSum,sum,li,a);
-34        a.remove(a.size()-1);
-35    }
-36}
+23}
